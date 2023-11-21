dfac39ec166c852153ba5b430fdb7b2eec480013
TanTN12 <ttan6596@gmail.com>
2023-11-21 17:19:02 +0700

    add

--- src/middlewares/handleError.js
+++ src/middlewares/handleError.js
@@ -4,7 +4,7 @@ const notfound = (req,res,next) => {
     next(error)
 }
 const handleError = (error, req, res, next) => {
-    const status = res.statusCode === 200 ? 500 : res.statusCode
+    const status = res.statusCode === 200 ? 200 : res.statusCode
     res.status(status).json({
         success: false,
         message: error.message
