654029707029a95efffc439ce81e775d6fd4b9ea
TanTN12 <ttan6596@gmail.com>
2023-12-17 01:33:25 +0700

    update

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,15 +1,15 @@
 import "dotenv/config"
 
 export const env = {
-    MONGODB_URI: process.env.MONGODB_URI,
-    APP_PORT: process.env.APP_PORT || 8000,
-    APP_HOST: process.env.APP_HOST || 'localhost',
-    SECRET_KEY: process.env.SECRET_KEY,
-    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD,
-    EMAIL_NAME: process.env.EMAIL_NAME,
-    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
-    CLOUDINARY_KEY:process.env.CLOUDINARY_KEY,
-    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
-    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
-    PAGE_LIMIT: process.env.PAGE_LIMIT,
+    MONGODB_URI: process.env.VERCEL_MONGODB_URI,
+    APP_PORT: process.env.VERCEL_APP_PORT || 8000,
+    APP_HOST: process.env.VERCEL_APP_HOST || 'localhost',
+    SECRET_KEY: process.env.VERCEL_SECRET_KEY,
+    EMAIL_APP_PASSWORD: process.env.VERCEL_EMAIL_APP_PASSWORD,
+    EMAIL_NAME: process.env.VERCEL_EMAIL_NAME,
+    CLOUDINARY_NAME: process.env.VERCEL_CLOUDINARY_NAME,
+    CLOUDINARY_KEY:process.env.VERCEL_CLOUDINARY_KEY,
+    CLOUDINARY_SECRET: process.env.VERCEL_CLOUDINARY_SECRET,
+    CLIENT_URL: process.env.VERCEL_CLIENT_URL || "http://localhost:3000",
+    PAGE_LIMIT: process.env.VERCEL_PAGE_LIMIT,
 }
