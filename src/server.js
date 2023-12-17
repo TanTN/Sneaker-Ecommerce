3741931cc3c34c88b74f5af4d7800269dcea1a50
TanTN12 <ttan6596@gmail.com>
2023-12-17 10:40:54 +0700

    update

--- src/server.js
+++ src/server.js
@@ -31,7 +31,6 @@ const ON_SERVER = () => {
         credentials: true,
         allowedHeaders: 'Content-Type,Authorization'
     }))
-    // app.use(cors())
     routes(app)
     
     const PORT = env.APP_PORT || 6100
